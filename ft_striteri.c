1d936fa70956154a6898d6f7c939b5ea2da68b32
Saad Akllam <sakllam@e2r3p13.1337.ma>
2021-11-04 18:46:37 +0100

    go1

--- ft_striteri.c
+++ ft_striteri.c
@@ -13,18 +13,18 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		i++;
 	}
 }
-void ft_test(unsigned int c, char *b)
-{
-	*b = c + *b;
-}
+// void ft_test(unsigned int c, char *b)
+// {
+// 	*b = c + *b;
+// }
 
-#include <stdio.h>
-int main()
-{
-	char *b = malloc(sizeof(char) *4);
-	b[0] = '1';
-	b[1] = '2';
-	b[2] = '3';
-	ft_striteri(b, &ft_test);
-	printf("%s", b);
-}
+// #include <stdio.h>
+// int main()
+// {
+// 	char *b = malloc(sizeof(char) *4);
+// 	b[0] = '1';
+// 	b[1] = '2';
+// 	b[2] = '3';
+// 	ft_striteri(b, &ft_test);
+// 	printf("%s", b);
+// }
